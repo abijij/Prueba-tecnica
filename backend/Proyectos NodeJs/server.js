3b5af91241df2646f8f3ddd524b3bb9f40c9c0fb
abijij <105397465+abijij@users.noreply.github.com>
2024-03-08 07:48:25 -0600

    Imoplements

--- backend/Proyectos NodeJs/server.js
+++ backend/Proyectos NodeJs/server.js
@@ -2,32 +2,29 @@ const express = require('express');
 const https = require('https');
 const fs = require('fs');
 const path = require('path');
-const cors = require('cors');
-const multer = require('multer');
 
 const app = express();
 const logger = require('morgan');
+const cors = require('cors');
+const multer = require('multer');
+
 const vehiculosSocket = require('./sockets/vehiculoSockets');
+
 const vehiculosRoutes = require('./routes/VehiculosRoutes');
 
 const port = process.env.PORT || 443;
 
 app.use(logger('dev'));
-
-// Configuración para permitir todos los orígenes
-app.use(cors({
-  origin: '*',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-  optionsSuccessStatus: 204,
-}));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 app.disable('x-powered-by');
+
 app.set('port', port);
 
 const upload = multer({ storage: multer.memoryStorage() });
+
+
 
 vehiculosRoutes(app, upload);
 
@@ -41,24 +38,31 @@ const credentials = { key: privateKey, cert: certificate };
 
 const server = https.createServer(credentials, app);
 
-const io = require('socket.io')(server);
+const io = require('socket.io')(server, {
+    cors: {
+      origin: 'https://prueba-tecnica-khaki.vercel.app',
+      methods: ['GET', 'POST'],
+      credentials: true,
+    },
+  });
 
 vehiculosSocket(io);
 
 server.listen(port, '172.26.6.212', function () {
-  console.log('Listening on port ' + port + ' with HTTPS');
+    console.log('Listening on port ' + port + ' with HTTPS');
 });
 
+
 app.use((err, req, res, next) => {
-  console.log(err);
-  res.status(err.status || 500).send(err.stack);
+    console.log(err);
+    res.status(err.status || 500).send(err.stack);
 });
 
 app.get('/', (req, res) => {
-  res.send('Root route of the backend');
+    res.send('Root route of the backend');
 });
 
 module.exports = {
-  app: app,
-  server: server
+    app: app,
+    server: server
 };
--- frontend/vehiculos-project/src/utils/SocketIo.tsx
+++ frontend/vehiculos-project/src/utils/SocketIo.tsx
@@ -1,4 +1,11 @@
 import io from 'socket.io-client';
 
-const socket = io('https://18.190.64.227/vehiculo/loc');
+const socket = io('https://18.190.64.227/vehiculo/loc', {
+  withCredentials: true,
+  extraHeaders: {
+    "Access-Control-Allow-Origin": "https://prueba-tecnica-khaki.vercel.app",
+    
+  },
+});
+
 export default socket;
